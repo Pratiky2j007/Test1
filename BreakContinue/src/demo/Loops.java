f769398e4062a70d9c76440539dd8925f38761ad
pkumar <pkumar@pkumar-lt.wk.pune>
2018-09-28 13:06:40 +0530

    adding a new project

---
+++ BreakContinue/src/demo/Loops.java
@@ -0,0 +1,41 @@
+package demo;
+
+import java.util.*;
+
+public class Loops 
+{
+	public void BreakLoop()
+	{
+		ArrayList<String> arrlist=new ArrayList<String>();
+		arrlist.add("a2");
+		arrlist.add("a3");
+		arrlist.add("a4");
+		arrlist.add("a5");
+		for(int i=0;i<arrlist.size();i++)
+		{
+			if(i==2)
+			{
+				break;
+			}
+			System.out.println(arrlist.get(i));
+		}
+	}
+	
+	public void ContinueLoop()
+	{
+		ArrayList<String> arrlist=new ArrayList<String>();
+		arrlist.add("a2");
+		arrlist.add("a3");
+		arrlist.add("a4");
+		arrlist.add("a5");
+		for(int i=0;i<arrlist.size();i++)
+		{
+			if(i==2)
+			{
+				continue;
+			}
+			System.out.println(arrlist.get(i));
+		}
+	}
+
+}
---
+++ BreakContinue/src/demo/MainClass.java
@@ -0,0 +1,12 @@
+package demo;
+
+public class MainClass {
+
+	public static void main(String[] args) 
+	{
+		Loops obj=new Loops();
+		//obj.BreakLoop();
+		obj.ContinueLoop();
+	}
+
+}
